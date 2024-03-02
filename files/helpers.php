1694ac1c9c98ce366315c36a747015d8e3c91857
Tiago França <tiago@tiagofranca.com>
2024-03-01 23:44:56 -0300

    created is_unserializable, try_unserialize helpers
    On branch 3.x-dev
    	modified:   composer.json
    	new file:   files/helpers.php

---
+++ files/helpers.php
@@ -0,0 +1,66 @@
+<?php
+
+if (!function_exists('is_unserializable')) {
+    /**
+     * function is_unserializable
+     *
+     * @param mixed $data
+     *
+     * @return bool
+     */
+    function is_unserializable(mixed $data): bool
+    {
+        $data = is_string($data) ? trim($data) : null;
+
+        if (!$data || !(strlen($data) === 2 && $data === 'N;') || strlen($data) < 4) {
+            return false;
+        }
+
+        foreach (['N;', ';', ':'] as $partial) {
+            if (
+                (
+                    str_contains($data[1] ?? '', $partial)
+                    || str_contains($data, $partial)
+                ) && (
+                    str_ends_with($data, ';')
+                    || str_ends_with($data, '}')
+                )
+            ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('try_unserialize')) {
+    /**
+     * function try_unserialize
+     *
+     * @param mixed $data
+     * @param mixed $defaultOnFail
+     * @param null|\Closure $catcher
+     *
+     * @return mixed
+     */
+    function try_unserialize(
+        mixed $data,
+        mixed $defaultOnFail = null,
+        ?\Closure $catcher = null
+    ): mixed {
+        try {
+            if (!is_unserializable($data)) {
+                return $defaultOnFail;
+            }
+
+            return unserialize($data);
+        } catch (\Throwable $th) {
+            if ($catcher) {
+                $catcher($th);
+            }
+
+            return $defaultOnFail;
+        }
+    }
+}
